fbf130f15744e33782d74f6d5dda053b54019bb3
oddyec <diegomarquez2008@outlook.com>
2021-04-26 10:08:14 -0500

    css webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -19,6 +20,11 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader,
+        'css-loader']
+      },
     ],
   },
   plugins : [
@@ -26,6 +32,7 @@ module.exports = {
       inject: true,
       template: './public/index.html',
       filename: './index.html'
-    })
+    }),
+    new MiniCssExtractPlugin(),
   ]
 };
